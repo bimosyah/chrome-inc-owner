7d21542794fa232f27ef7ec1ed7340834f21e127
bimosyah <bimosyahputro88@gmail.com>
2020-06-15 12:00:43 +0700

    first

---
+++ app/src/main/java/bimo/syahputro/chromeincowner/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package bimo.syahputro.chromeincowner.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
